1fd44bbba98c45a0721219807f401ec14cd6d163
mario <kinuthiamariodavis@gmail.com>
2023-09-23 15:59:02 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Write your solution here!
+// Write your solution here!
+const cats =["Milo", "Otis", "Garfield"];
+
+
+function destructivelyAppendCat(name){
+
+    return cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+    return cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(){
+    return cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    return cats.shift();
+}
+
+function appendCat(name){
+    const newCat = [...cats,"Broom"];
+    return newCat;  
+}
+
+function prependCat(name){
+
+    const newCat = ["Arnold",...cats];
+    return newCat; 
+}
+
+function removeLastCat(){
+    
+    return cats.slice(0,-1);
+}
+
+function removeFirstCat(){
+
+    
+    return cats.slice(1);
+
+}
